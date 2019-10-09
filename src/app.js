6b985748549894f02af8197743a9e2313ed103e7
messiasGeovani <messias.geovani.lima@gmail.com>
2019-10-09 17:17:10 -0300

    app file created

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const Express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const app = Express()
+
+/**
+ * Database setup
+ */
+mongoose.connect(
+    'mongodb+srv://geovani:geovani123@cluster0-jvwp7.mongodb.net/grillo?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+    }
+)
+
+// BodyParser config
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }))
+// morgan config
+app.use(morgan('dev'))
+
+// routes init
+app.use(require('./routes/user'))
+app.use(require('./routes/files'))
+app.use(require('./routes/posts'))
+app.use(require('./routes/eventos'))
+app.use(require('./routes/vagas'))
+app.use(require('./routes/musicos'))
+app.use(require('./routes/mensagens'))
+
+// server run config
+app.listen(3000, () => {
+    console.log('Listenning at :3000')
+})
