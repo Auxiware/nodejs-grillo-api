46f67a9284c4c3ccabf21dcbe53c114627afcce5
messiasGeovani <messias.geovani.lima@gmail.com>
2019-11-07 15:26:16 -0300

    code chenge

--- src/app.js
+++ src/app.js
@@ -1,41 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
+// cors import
+const cors = require('cors')
+// routes import
+const routes = require('./routes').Router
 
-class App {
-    constructor() {
-        // starting express app
-        this.express = express()
-
-        // starting methods
-        this.middlewares()
-        this.routes()
-    }
-
-    // middlewares start method
-    middlewares() {
-        // json body support config
-        this.express.use(express.json())
-        this.express.use(
-            express.urlencoded({
-                extended: true
-            })
-        )
-        // morgen log start
-        this.express.use(morgan('dev'))
-        // database connection start
-        const database = require('./config/database').Db
-        database()
-
-        this.express.use(require('cors')())
-    }
-
-    // routes start method
-    routes() {
-        // starting the routes
-        const routes = require('./routes').Router
-        this.express.use(routes)
-    }
-}
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// morgan logs
+app.use(morgan('dev'))
+// cors config
+app.use(cors())
+// routes config
+app.use(routes)
 
 // exporting the app
-exports.App = new App().express
+exports.App = app
--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 // app object import
 const app = require('./app').App
 
+// database connect
+mongoose.connect(
+    'mongodb+srv://dev:1234@cluster0-jvwp7.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+
 // starting the server
 app.listen(8080)
 console.log('Application server is running!')
