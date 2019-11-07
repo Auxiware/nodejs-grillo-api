316aeaaea71b53a8c7ac6600ce1be836c9463130
messiasGeovani <messias.geovani.lima@gmail.com>
2019-11-07 15:27:03 -0300

    models simplify

--- src/App/models/index.js
+++ src/App/models/index.js
@@ -2,9 +2,6 @@ const model = require('mongoose').model
 
 module.exports = {
     Profile: model('profile', require('./profile').ProfileSchema),
-    Musician: model('musician', require('./musician').MusicianSchema),
     Post: model('post', require('./posts').PostSchema),
-    Event: model('event', require('./event').EventSchema),
-    Vacancy: model('vacancy', require('./vacancy').VacancySchema),
     Chat: model('chat', require('./chat').ChatSchema)
 }
